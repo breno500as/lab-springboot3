3e2f3d320a2de58f9e98eb751d85ac5fa649523d
breno.mcosta <breno.mcosta@pbh.gov.br>
2023-09-21 15:04:21 -0300

    init

---
+++ src/main/java/com/br/labspringboot3/LabSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.br.labspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpringboot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/labspringboot3/LabSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.labspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
