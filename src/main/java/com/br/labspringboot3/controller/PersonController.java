cbf1123bb50a94bd958e937bb1bc33f2b554653a
breno.mcosta <breno.mcosta@pbh.gov.br>
2023-09-22 14:08:23 -0300

    crud

--- src/main/java/com/br/labspringboot3/controller/PersonController.java
+++ src/main/java/com/br/labspringboot3/controller/PersonController.java
@@ -2,9 +2,14 @@ package com.br.labspringboot3.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +28,26 @@ public class PersonController {
 	public ResponseEntity<PersonDTO> create(@RequestBody PersonDTO person) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.personService.save(person));
 	}
+	
+	@PutMapping
+	public ResponseEntity<PersonDTO> update(@RequestBody PersonDTO person) {
+		return ResponseEntity.ok().body(this.personService.update(person));
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<PersonDTO> findById(@PathVariable(name = "id") Long id) {
+		return ResponseEntity.ok().body(this.personService.findById(id));
+	}
 
 	@GetMapping
 	public ResponseEntity<Iterable<PersonDTO>> findAll() {
 		return ResponseEntity.ok().body(this.personService.findAll());
+	}
+
+	@DeleteMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> delete(@PathVariable(name = "id") Long id) {
+		this.personService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/br/labspringboot3/service/PersonService.java
+++ src/main/java/com/br/labspringboot3/service/PersonService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.br.labspringboot3.dto.PersonDTO;
 import com.br.labspringboot3.entity.PersonEntity;
+import com.br.labspringboot3.exception.ExpectationFailedException;
 import com.br.labspringboot3.repository.PersonRepository;
 
 @Service
@@ -22,15 +23,51 @@ public class PersonService {
 
 	public PersonDTO save(PersonDTO person) {
 		this.log.info("save person");
+
 		final PersonEntity personEntity = this.personRepository.save(new PersonEntity(person.getName()));
 		return new PersonDTO(personEntity.getId(), personEntity.getName());
 	}
 
+	public PersonDTO update(PersonDTO person) {
+
+		this.log.info("update person");
+
+		final PersonEntity pEntity = this.findEntityById(person.getId());
+
+		pEntity.setName(person.getName());
+
+		final PersonEntity updatedPerson = this.personRepository.save(pEntity);
+
+		return new PersonDTO(updatedPerson.getId(), updatedPerson.getName());
+
+	}
+
 	public List<PersonDTO> findAll() {
 		this.log.info("find all persons");
+		
 		return StreamSupport.stream(this.personRepository.findAll().spliterator(), false)
 				.map(p -> new PersonDTO(p.getId(), p.getName())).toList();
 
+	}
+
+	public void delete(Long id) {
+		this.log.info("delete person");
+
+		this.personRepository.deleteById(id);
+	}
+
+	private PersonEntity findEntityById(Long id) {
+		return this.personRepository.findById(id)
+				.orElseThrow(() -> new ExpectationFailedException("person not found!"));
+	}
+
+	public PersonDTO findById(Long id) {
+
+		this.log.info("find person by id");
+
+		final PersonEntity p = this.findEntityById(id);
+
+		return new PersonDTO(p.getId(), p.getName());
 	}
 
 }
